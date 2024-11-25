04b7284c3dc338bea49e7bffcd58d81f979f663a
massimopallera <pallera.massimo@gmail.com>
2024-11-25 10:17:52 +0100

    created routes

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,23 @@
+import logo from '../assets/react.svg'
+
+
+export default function () {
+  return (
+    <>
+    <header header >
+        <div className="logo">
+          <img src={logo} alt="" />
+    </div>
+    <nav>
+      <a href="/">Home</a>
+      <a href="/books">Books</a>
+      <a href="/about">About</a>
+      <a href="/contacts">Contacts</a>
+    </nav>
+    </header>
+    
+      <main><h1>CONTACTS</h1></main>
+      </>
+
+  )
+}
