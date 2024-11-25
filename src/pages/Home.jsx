9d2516c6ba401530f434b0307c048e8403b544fc
massimopallera <pallera.massimo@gmail.com>
2024-11-25 10:27:13 +0100

    add: footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,12 @@ export default function () {
       
       <main>
         <h1>HOME</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At nobis aliquam optio culpa. Magni eligendi labore nostrum iusto illo tenetur aut, facilis, commodi, nulla molestiae distinctio incidunt quo alias at.</p>
       </main>
+
+      <footer>
+        <p>copyright 2024</p>
+      </footer>
     </>
 
   )
